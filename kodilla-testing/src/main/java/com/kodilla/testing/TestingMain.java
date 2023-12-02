8228beeb08e87a13a8c52a6f0ce7cf1b3671c8fa
Piotr Merecki <yamakiteo@gmail.com>
2023-12-02 19:58:09 +0100

    Calculator Testing

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,29 @@
 package com.kodilla.testing;
 
-public class TestingMain {
-    public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+import com.kodilla.testing.user.SimpleUser;
+
+import com.kodilla.testing.calculator.Calculator;
+
+    public class TestingMain {
+
+        public static void main(String[] args) {
+            SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+            String result = simpleUser.getUsername();
+
+            if (result.equals("theForumUser")) {
+                System.out.println("test OK");
+            } else {
+                System.out.println("Error!");
+            }
+
+            Calculator calculator = new Calculator();
+
+            int add = calculator.add(5, 7);
+            int subtract = calculator.subtract(30, 10);
+
+            System.out.println(add);
+            System.out.println(subtract);
+
+        }
     }
-}
