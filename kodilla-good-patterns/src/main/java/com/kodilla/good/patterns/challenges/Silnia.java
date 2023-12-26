ea990fbd700de7f2257eb0043e3625df06b759cc
Piotr Merecki <yamakiteo@gmail.com>
2023-12-26 20:56:10 +0100

    9.1 Dodatek

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Silnia.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Silnia.java
@@ -4,7 +4,7 @@ class Silnia {
         public static void main(String[] args){
             int i, fact = 1;
             int number = 5;
-            for(i = 1;i <= number; i++){
+            for(i = 1; i <= number; i++){
                 fact = fact * i;
             }
             System.out.println("Factorial of " + number + " is: " + fact);
