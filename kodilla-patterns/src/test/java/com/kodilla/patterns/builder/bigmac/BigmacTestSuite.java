e07c8fbc0ca05e7608f3285f32cd08ddeff422c3
Piotr Merecki <yamakiteo@gmail.com>
2024-01-07 19:01:32 +0100

    11.5

--- kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -1,9 +1,28 @@
 package com.kodilla.patterns.builder.bigmac;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+@DisplayName("Big Mac Test Suite")
 public class BigmacTestSuite {
+    private static int testCounter = 0;
+
+    @BeforeAll
+    public static void beforeAllTests() {
+        System.out.println("This is the beginning of tests.");
+    }
+
+    @AfterAll
+    public static void afterAllTests() {
+        System.out.println("All tests are finished.");
+    }
+
+    @BeforeEach
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("Preparing to execute test #" + testCounter);
+    }
+
+    @DisplayName("Test New Big Mac")
     @Test
     public void testNewBigMac() {
         //Given
